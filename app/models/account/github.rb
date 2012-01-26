cf77f445aa4545b4229feb0ed6fd9fb28c6df951
makaroni4 <makaroni4@gmail.com>
2012-01-27 01:05:45 +0300

    Added testless post functionality

--- app/models/account/github.rb
+++ app/models/account/github.rb
@@ -1,22 +1,5 @@
 class Account::Github < ActiveRecord::Base
-  API_URL = "https://api.github.com"
   
   belongs_to :user
   
-  def get_gists
-    uri = URI.parse("#{API_URL}/users/#{self.user.username}/gists")
-    get_data(uri)
-  end
-  
-  private 
-  
-  def get_data(uri)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request).body
-    ActiveSupport::JSON.decode(response)
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
   
+  belongs_to :user
+  default_scope :order => 'created_at DESC'
+  
+  def parsed_body
+    CGI::escapeHTML(body).gsub(/\{gist:(\d+)\}/) { Github::Gist.script_tag($1) }
+  end
 end
