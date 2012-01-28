9675c370a97d39815fb5c607c1bb34c8e96ec938
makaroni4 <makaroni4@gmail.com>
2012-01-29 01:31:47 +0300

    Added boolean field question to comments (question comments will be highlighted)

--- db/migrate/20120125165435_create_comments.rb
+++ db/migrate/20120125165435_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :type
+      t.boolean :question
       t.text :body
       t.integer :author_id
       t.integer :consignee_id
