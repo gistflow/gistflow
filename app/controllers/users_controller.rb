26b2a9c0b4ee8366c65ec2211302c79f28d89889
Jan Bernacki <releu@me.com>
2012-01-31 14:38:26 +0400

    add logger for omniauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def create
+    Rails.logger.info "[OMNI] #{omniauth.inspect}"
     account = Account::Github.find_or_create_by_omniauth(omniauth)
     self.current_user = account.user
     redirect_to root_path
