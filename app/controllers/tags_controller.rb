622b459180c651dcfc28cd752b712c431f9bc1a0
Anatoli Makarevich <makaroni4@gmail.com>
2012-02-10 20:38:19 +0300

    FIX tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,6 @@ class TagsController < ApplicationController
     @tag = Tag.find_by_name(params[:id])
     @posts = Post.tagged_with(@tag.name).page(params[:page])
     @subscription = current_user.subscriptions.
-      find_or_initialize_by_tag_id(@tag.id)
+      find_or_initialize_by_tag_id(@tag.id) if current_user
   end
 end
