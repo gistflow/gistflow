1934837b3e966f29598d9dbd84dbcc77b01fc054
Jan Bernacki <releu@me.com>
2012-02-12 14:55:42 +0400

    styles

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,24 @@ Gistflow::Application.routes.draw do
     match :like, :on => :member, :via => :post
     match :add_to_favorites, :on => :member, :via => :post
     resources :comments, :only => :create
+    
+    collection do
+      resources :articles, {
+        :only => [:index, :show],
+        :controller => :posts, 
+        :type => 'Post::Article'
+      }
+      resources :questions, {
+        :only => [:index, :show], 
+        :controller => :posts, 
+        :type => 'Post::Question'
+      }
+      resources :community, {
+        :only => [:index, :show], 
+        :controller => :posts, 
+        :type => 'Post::Community'
+      }
+    end
   end
   
   resources :tags, :only => :show
@@ -16,19 +34,7 @@ Gistflow::Application.routes.draw do
   resources :subscriptions, :only => [:create, :destroy]
   resources :users, :only => :show
   resources :gists, :only => [:show, :index]
-  
-  resources :articles, 
-    :only => [:index, :show], 
-    :controller => :posts, 
-    :type => 'Article'
-  resources :questions,
-    :only => [:index, :show], 
-    :controller => :posts, 
-    :type => 'Question'
-  resources :community, 
-    :only => [:index, :show], 
-    :controller => :posts, 
-    :type => 'Community'
   resources :tags, :only => :show
+  
   root to: 'posts#index'
 end
