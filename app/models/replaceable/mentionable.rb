582643453aa4b8011fe040f8c0b29a7e01f68091
Anatoli Makarevich <makaroni4@gmail.com>
2012-02-12 21:25:44 +0300

    FIX user link in replaceable::mentionable

--- app/models/replaceable/mentionable.rb
+++ app/models/replaceable/mentionable.rb
@@ -6,7 +6,7 @@ module Replaceable
       usernames = Parser::Mention.new(self.content).usernames
       self.content = self.content.split(/ /).map do |word|
         if username = word.scan(/^\W*@([\w-]+)/).flatten.first and usernames.include?(username)
-          link_to("@#{username}", "users/#{username}").html_safe
+          link_to("@#{username}", "/users/#{username}").html_safe
         else
           word
         end
