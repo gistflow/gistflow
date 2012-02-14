63697e683c36a0c4f80d777d1b8aaa16d9f9b9b9
Jan Bernacki <releu@me.com>
2012-02-14 23:21:31 +0400

    remove unused helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,15 +39,7 @@ module ApplicationHelper
         :class => 'button icon like', :method => :post
     end
   end
-  
-  def user_gists_title(user)
-    title = user == current_user ? "Your gists" : "#{user.username} on Github"
     
-    capture_haml do
-      caption_haml title
-    end
-  end
-  
   def caption_haml(title)
     haml_tag :div, :class => 'caption' do
       haml_concat title
@@ -60,24 +52,7 @@ module ApplicationHelper
       caption_haml "#{name} posts"
     end
   end
-  
-  def sidebar_posts(title, posts, more_url)
-    capture_haml do
-      caption_haml title
-
-      posts.each do |post|
-        haml_tag :div, :class => 'sidebar_link' do
-          #FIX post.title from content
-          #FIX post_path to models_path
-          haml_concat link_to(post.content[0..30], post_path(post)).html_safe
-        end
-      end
-      haml_tag :div, :class => 'sidebar_link' do
-        haml_concat link_to "more", more_url
-      end
-    end
-  end
-  
+    
   def link_to_notifiable(notification)
     username = notification.notifiable.user.username
     user_link = link_to(
