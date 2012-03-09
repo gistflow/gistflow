6069b8509c6047cb745b8e12cae6a4ba3ab51d57
Jan Bernacki <releu@me.com>
2012-03-09 22:35:36 +0400

    move controllers to namespace

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,15 +1,5 @@
 class GistsController < ApplicationController
-  before_filter :authenticate!, :only => :index
   respond_to :json
-  
-  def index
-    options = {
-      :partial => 'sidebar.html.haml',
-      :layout => false,
-      :locals => { :load_gists => true }
-    }
-    respond_with div: render_to_string(options)
-  end
   
   def show
     @gist = Github::Gist.find(params[:id])
