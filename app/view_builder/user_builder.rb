fc7be493f27fe24dc5321d2f72dd4eb9928c4f0c
Jan Bernacki <releu@me.com>
2012-03-09 16:55:29 +0400

    add users sidebar

---
+++ app/view_builder/user_builder.rb
@@ -0,0 +1,19 @@
+class UserBuilder < ViewBuilder
+  def title
+    super(options[:self] ? "Your Profile" : "#{user}'s profiles")
+  end
+  
+  def link_to_github
+    link_to "#{user} on Github", "https://github.com/#{username}"
+  end
+  
+protected
+  
+  def show?
+    params[:controller] == 'users'
+  end
+  
+  def highlight?
+    true
+  end
+end
