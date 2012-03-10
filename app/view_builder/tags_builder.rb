107123a618106e5680d23bd4156583e60836f935
Anatoli Makarevich <makaroni4@gmail.com>
2012-03-10 20:59:09 +0300

    Added tags sidebar block

---
+++ app/view_builder/tags_builder.rb
@@ -0,0 +1,25 @@
+class TagsBuilder < ViewBuilder
+  def title
+    super('Tags')
+  end
+  
+  def tags
+    content_tag(:ol) do
+      if user.tags.any?
+        user.tags.map do |tag|
+          content_tag(:li) do
+            link_to(tag, url_helpers.tag_path(:id => tag.name))
+          end
+        end.join.html_safe
+      else
+        link_to "Subscribe for tags", url_helpers.account_subscriptions_path
+      end
+    end
+  end
+  
+protected
+  
+  def wrap_class
+    'sidebar_tags'
+  end
+end
