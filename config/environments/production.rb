880d6829dcb002f2997ceaaea381c9865afb6a68
Anatoli Makarevich <makaroni4@gmail.com>
2012-03-11 19:47:31 +0300

    Added mailgun to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,15 @@ Gistflow::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'], 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable threaded mode
   # config.threadsafe!
