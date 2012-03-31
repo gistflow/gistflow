c7c574c73525386acc22f104cd2a7861a5649247
Anatoli Makarevich <makaroni4@gmail.com>
2012-04-01 00:11:54 +0400

    Try to run assests on heroku

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,6 @@ module Gistflow
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
     config.assets.manifest = Rails.root.join("public/assets")
     
     # Version of your assets, change this if you want to expire all your assets
