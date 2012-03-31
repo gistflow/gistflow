910f0c99afd9d0cc4d5f27cfdce575801cba1920
Jan Bernacki <releu@me.com>
2012-03-31 22:23:49 +0400

    recompile accets

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,14 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  # Bundler.require *Rails.groups(:assets => %w(development test))
+  Bundler.require *Rails.groups(:assets => %w(development test))
   # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module Gistflow
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
