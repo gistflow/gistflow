b3518303c81ea16f5473baef66d6274a480777a4
Jan Bernacki <releu@me.com>
2012-04-26 20:33:34 +0400

    fix tags and text area height

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :posts
   has_many :subscriptions
   
-  validates :name, :presence => true
+  validates :name, :presence => true, :format => { :with => /[a-z]+/ }
   
   scope :popular, (lambda do |limit = 100|
     order('posts_count desc').limit(limit)
@@ -10,6 +10,12 @@ class Tag < ActiveRecord::Base
   
   def to_s
     name
+  end
+  
+  def name=(name)
+    name.to_s.gsub!(/[\-_]/, '')
+    name.downcase!
+    write_attribute :name, name
   end
   
   def with_sign
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe Tag do
+  let(:tag) { create(:tag) }
+  subject { tag }
+  
+  describe('#name=') do
+    subject { tag.name }
+    before { tag.name = tag_name }
+    
+    context 'with underscores' do
+      let(:tag_name) { 'ruby_on_rails' }
+      it { should == 'rubyonrails' }
+    end
+    
+    context 'uppercase' do
+      let(:tag_name) { 'RubyOnRails' }
+      it { should == 'rubyonrails' }
+    end
+    
+    context 'dashes' do
+      let(:tag_name) { 'ruby-on-rails' }
+      it { should == 'rubyonrails' }
+    end
+  end
 end
