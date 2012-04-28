172fb105df6a6d2da27e0453618fbb0301d8ef2c
Jan Bernacki <releu@me.com>
2012-04-28 17:55:45 +0400

    fix link to gist on preview

--- app/assets/javascripts/gists.js
+++ app/assets/javascripts/gists.js
@@ -6,7 +6,15 @@ $(function(){
       $('.importable-gist').tooltip({title: 'You can import this gist to new post.'});
     })
   }
-
+  
+  $("article.post a:contains('gist:')").click(function(){
+    var article = $(this).parents('article:first')
+    if (!article.hasClass('detail')) {
+      document.location = article.attr('rel')
+      return false
+    }
+  })
+  
   $("article.post.detail a:contains('gist:')").each(function(){
     var id = $(this).html().match(/gist:(\d+)/)[1],
       link = ("/gists/" + id + ".json"),
