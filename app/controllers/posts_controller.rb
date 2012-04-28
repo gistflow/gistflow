042c9366adba4c518739711cf2808b071035f8bb
Anatoli Makarevich <makaroni4@gmail.com>
2012-04-28 18:42:04 +0400

    Added hint for markdown

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,8 +26,7 @@ class PostsController < ApplicationController
   def new
     content = ""
     content << "gist:#{params[:gist_id]}" if params[:gist_id]
-    content << " ##{params[:gist_lang]}" if params[:gist_lang]
-    content << " "
+    content << " ##{params[:gist_lang]} " if params[:gist_lang]
     
     @post = Post.new(:content => content)
     @presenter = Posts::FormPresenter.new(@post)
