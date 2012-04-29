d0e3645a47f6ade231a06bb59e14a2b8e903a271
Anatoli Makarevich <makaroni4@gmail.com>
2012-04-29 17:45:40 +0400

    Added at-username jQuqery plugin and autocomplete for usernames in comments

--- app/presenters/posts/show_presenter.rb
+++ app/presenters/posts/show_presenter.rb
@@ -41,6 +41,10 @@ class Posts::ShowPresenter
   end
   
   def comments
-    post.comments.to_a.select(&:persisted?)
+    post.comments.includes(:user).to_a.select(&:persisted?)
+  end
+  
+  def usernames
+    [*comments.map{ |c| c.user.username }, post.user.username].uniq.sort
   end
 end
