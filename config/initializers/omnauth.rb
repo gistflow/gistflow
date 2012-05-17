8e6c4899ba6baf54ad63889c7252aa11aa81f05b
Jan Bernacki <releu@me.com>
2012-05-17 23:38:15 +0400

    fix ssl

--- config/initializers/omnauth.rb
+++ config/initializers/omnauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'gist'
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'gist', client_options: { ssl: { ca_path: "/etc/ssl/certs" } }
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], client_options: { ssl: { ca_path: "/etc/ssl/certs" } }
 end
