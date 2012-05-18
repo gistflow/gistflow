4e464d3ea05986460c55f37f6b7e841e1c880c3b
Anatoli Makarevich <makaroni4@gmail.com>
2012-05-18 21:48:00 +0400

    Remove observing wall from settings

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,7 +1,7 @@
 class Settings < ActiveRecord::Base
   attr_accessible :default_wall
   
-  WALLS = %w(all flow following observing)
+  WALLS = %w(all flow following)
   
   belongs_to :user
   validates :default_wall, :inclusion => { :in => WALLS }
