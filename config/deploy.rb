6f47e1c9a101ce0c8b389389fc0e2d89cb90b67d
Jan Bernacki <releu@me.com>
2012-05-21 00:19:11 +0400

    add configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,31 @@
-set :rvm_ruby_string, '1.9.3@gistflow'
+require 'bundler/capistrano'
+
+$:.unshift(File.expand_path("./lib", ENV["rvm_path"]))
 require "rvm/capistrano"
 
-require 'bundler/capistrano'
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :root
+set :stage, 'uk'
+
+set :use_sudo, true
+
+set :server, :unicorn
 
 set :application, "gistflow"
 set :repository,  "git@github.com:gistflow/gistflow.git"
 
 set :scm, :git
 
-set :user, "gistflow"
+set :user, "git"
 
-role :web, "gistflow.com"
-role :app, "gistflow.com"
-role :db,  "gistflow.com", :primary => true
+role :web, "37.188.125.88"
+role :app, "37.188.125.88"
+role :db,  "37.188.125.88", :primary => true
 
 after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
   task :restart do
-    run "cd #{current_release} && rvmsudo god restart unicorn"
+    run "cd #{current_release} && god -c #{current_release}/config/god.rb restart unicorn"
   end
 end
-
---
+++ config/god.rb
@@ -0,0 +1,59 @@
+rails_env = ENV['RAILS_ENV'] || 'production'
+rails_root = ENV['RAILS_ROOT'] || "/u/apps/gistflow/current/"
+
+God.watch do |w|
+  w.name = "unicorn"
+  w.interval = 30.seconds # default
+  
+  w.env = YAML.load_file "/home/git/config.yml"
+
+  # unicorn needs to be run from the rails root
+  w.start = "cd #{rails_root} && bundle exec unicorn_rails -c #{rails_root}/config/unicorn.rb -E #{rails_env} -D"
+
+  # QUIT gracefully shuts down workers
+  w.stop = "kill -QUIT `cat #{rails_root}/tmp/pids/unicorn.pid`"
+
+  # USR2 causes the master to re-create itself and spawn a new worker pool
+  w.restart = "kill -USR2 `cat #{rails_root}/tmp/pids/unicorn.pid`"
+
+  w.start_grace = 10.seconds
+  w.restart_grace = 10.seconds
+  w.pid_file = "#{rails_root}/tmp/pids/unicorn.pid"
+
+  w.uid = 'git'
+  w.gid = 'git'
+
+  w.behavior(:clean_pid_file)
+
+  w.start_if do |start|
+    start.condition(:process_running) do |c|
+      c.interval = 5.seconds
+      c.running = false
+    end
+  end
+
+  w.restart_if do |restart|
+    restart.condition(:memory_usage) do |c|
+      c.above = 300.megabytes
+      c.times = [3, 5] # 3 out of 5 intervals
+    end
+
+    restart.condition(:cpu_usage) do |c|
+      c.above = 50.percent
+      c.times = 5
+    end
+  end
+
+  # lifecycle
+  w.lifecycle do |on|
+    on.condition(:flapping) do |c|
+      c.to_state = [:start, :restart]
+      c.times = 5
+      c.within = 5.minute
+      c.transition = :unmonitored
+      c.retry_in = 10.minutes
+      c.retry_times = 5
+      c.retry_within = 2.hours
+    end
+  end
+end
