2de21cd0b31058fa943e4bc28b629ad72800175f
Jan Bernacki <releu@me.com>
2012-05-25 20:56:40 +0400

    link to subscribe

--- app/controllers/account/subscriptions_controller.rb
+++ app/controllers/account/subscriptions_controller.rb
@@ -11,14 +11,18 @@ class Account::SubscriptionsController < ApplicationController
   def create
     @tag = find_tag(params[:tag_id])
     current_user.subscribe(@tag)
-    link = render_to_string(inline: "<%= link_to_unsubscribe(@tag) %>")
+    link = render_to_string(
+      inline: "<%= link_to_unsubscribe(@tag, { onoff: params[:onoff] }) %>"
+    )
     render :json => { :replaceable => link }
   end
   
   def destroy
     @tag = find_tag(params[:tag_id])
     current_user.unsubscribe(@tag)
-    link = render_to_string(inline: "<%= link_to_subscribe(@tag) %>")
+    link = render_to_string(
+      inline: "<%= link_to_subscribe(@tag, { onoff: params[:onoff] }) %>"
+    )
     render :json => { :replaceable => link }
   end
   
