29418f1968fa4cf8903ffd4212ddbc57af5fa32d
Jan Bernacki <releu@me.com>
2012-05-25 22:38:05 +0400

    tips for user

--- app/controllers/account/subscriptions_controller.rb
+++ app/controllers/account/subscriptions_controller.rb
@@ -2,7 +2,6 @@ class Account::SubscriptionsController < ApplicationController
   cache_sweeper :subscription_sweeper
   
   before_filter :authenticate!, :only => :index
-  before_filter :setup_welcome_info, :if => :current_user_newbie?
   
   def index
     @tags = Tag.popular
@@ -30,15 +29,5 @@ protected
   
   def find_tag(name)
     Tag.find_by_name name
-  end
-  
-  def setup_welcome_info
-    flash.now[:info] = %{
-      <strong>#{current_user.username.capitalize}<strong>, welcome to Gistflow
-       - developers community based on sharing gists. Feel free to write 
-       articles with your github public gists, to use #hash_tags and mention 
-       other @users. <strong>And first - subscribe for some tags to get 
-       articles you are interested in.</strong> Good luck!
-    }.html_safe
   end
 end
