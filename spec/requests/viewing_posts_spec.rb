3b28b897d3d032d43fafc9f9d670658cd6e6111f
Jan Bernacki <releu@me.com>
2012-06-02 22:01:20 +0400

    fix title for non-authorized users

--- spec/requests/viewing_posts_spec.rb
+++ spec/requests/viewing_posts_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe 'Viewing posts', local: true do
-  context 'in flow' do
+  context 'on flow' do
     let!(:posts) { 30.times.map { create(:post) } }
     before { visit all_path }
   
     context 'as unauthorized user', js: true do
-      it 'should view 20 posts on page' do
+      it 'should show Post title' do
+        page.find('header.title h1').should have_content('Posts')
+      end
+      
+      it 'should show 20 posts on page' do
         Post.page(nil).each do |post|
           page.should have_css('article.post h1', text: post.title)
         end
@@ -24,6 +28,11 @@ describe 'Viewing posts', local: true do
   context 'on detail' do
     let(:post) { create(:post) }
     before { visit post_path(post) }
+    
+    it 'should show Post title' do
+      page.should_not have_css('header.title h1')
+      page.find('article.post header h1').should have_content(post.title)
+    end
     
     context 'as unauthorized user', js: true do
       include_examples 'inactive buttons'
