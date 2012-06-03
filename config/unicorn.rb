4b10ccd6ae6c0147a30b1b7a87003800f2529bca
Jan Bernacki <releu@me.com>
2012-06-03 20:29:05 +0400

    fix logs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,8 @@ listen '/u/apps/gistflow/shared/tmp/sockets/unicorn.sock'
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/path/to/app/shared/log/unicorn.stderr.log"
-stdout_path "/path/to/app/shared/log/unicorn.stdout.log"
+stderr_path "/u/apps/gistflow/shared/log/unicorn.stderr.log"
+stdout_path "/u/apps/gistflow/shared/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
