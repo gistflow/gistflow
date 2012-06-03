63cf476f39cc384ad58561debc9aa31db472d576
Jan Bernacki <releu@me.com>
2012-06-03 20:23:58 +0400

    add logs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,6 +5,17 @@ timeout 30
 pid '/u/apps/gistflow/shared/pids/unicorn.pid'
 listen '/u/apps/gistflow/shared/tmp/sockets/unicorn.sock'
 
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path "/path/to/app/shared/log/unicorn.stderr.log"
+stdout_path "/path/to/app/shared/log/unicorn.stdout.log"
+
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
