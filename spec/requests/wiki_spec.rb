b5ed4a14b517cff3aebf46ea79cd4894960bbbe2
Jan Bernacki <releu@me.com>
2012-06-05 15:07:15 +0400

    fix headers

---
+++ spec/requests/wiki_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'Wiki' do
+  let(:tag) { create(:tag) }
+  let(:wiki) { tag.wiki }
+  
+  describe 'Viewing wiki page' do
+    before { visit tag_wiki_path(tag) }
+    
+    it 'should have primary title' do
+      page.should have_css('header.primary h1', text: wiki.title)
+    end
+    
+    it "should show wiki's body" do
+      page.should have_content(wiki.body)
+    end
+  end
+  
+  describe 'Return to tag' do
+    before { visit tag_wiki_path(tag) }
+    
+    it 'should have a link to tag' do
+      page.should have_link 'Return to tag'
+    end
+    
+    it 'should show me tag page'
+  end
+  
+  describe 'History' do
+    before { visit tag_wiki_path(tag) }
+    
+    it 'should have a link to history' do
+      within('section.wiki') do
+        page.should have_link 'History'
+      end
+    end
+    
+    describe 'History page' do
+      
+    end
+    it 'should '
+  end
+end
