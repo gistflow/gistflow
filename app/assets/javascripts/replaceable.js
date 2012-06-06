92bfc5ace04dedb66c5d6ff9fa1ccba2817cd8f7
Anatoly Makarevich <makaroni4@gmail.com>
2012-06-07 01:02:57 +0400

    Added followers counter update when following/unfollowing

--- app/assets/javascripts/replaceable.js
+++ app/assets/javascripts/replaceable.js
@@ -1,5 +1,12 @@
 $(function(){
   $(document).on('ajax:success', '.replaceable', function(e, data){
-    $(this).replaceWith(data.replaceable)
+    $(this).replaceWith(data.replaceable);
+    var update_elements = data.update_elements;
+    if(update_elements){
+      _.each(update_elements, function(data, element){ 
+        console.log(element);
+        $('.' + element).text(data);
+      });
+    }
   })
 });
--- app/helpers/following_helper.rb
+++ app/helpers/following_helper.rb
@@ -1,11 +1,13 @@
 module FollowingHelper
   def followers_link user
-    title = "<b>#{user.followers.count}</b> Followers".html_safe
+    count = user.followers.count
+    text = 'Follower'.pluralize(count)
+    title = "<b class='followers_count'>#{count}</b> #{text}".html_safe
     link_to title, user_followers_path(user)
   end
   
   def followed_users_link user
-    title = "<b>#{user.followed_users.count}</b> Following".html_safe
+    title = "<b class='followings_count'>#{user.followed_users.count}</b> Following".html_safe
     link_to title, user_followings_path(user)
   end
 end
