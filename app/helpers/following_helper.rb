bda9035badaa26e8d06b1e6bbf150ad743aa229b
Anatoli Makarevich <makaroni4@gmail.com>
2012-06-07 23:21:02 +0400

    <b> to <strong>

--- app/helpers/following_helper.rb
+++ app/helpers/following_helper.rb
@@ -2,12 +2,12 @@ module FollowingHelper
   def followers_link user
     count = user.followers.count
     text = 'Follower'.pluralize(count)
-    title = "<b class='followers_count'>#{count}</b> #{text}".html_safe
+    title = "<strong class='followers_count'>#{count}</strong> #{text}".html_safe
     link_to title, user_followers_path(user)
   end
   
   def followed_users_link user
-    title = "<b class='followings_count'>#{user.followed_users.count}</b> Following".html_safe
+    title = "<strong class='followings_count'>#{user.followed_users.count}</strong> Following".html_safe
     link_to title, user_followings_path(user)
   end
 end
