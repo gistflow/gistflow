396978c3654e94ca689af0f1d1394eac08f1d9e9
Anatoly Makarevich <makaroni4@gmail.com>
2012-06-13 13:46:08 +0400

    FIX textarea username autocomplete - it is now insert username at caret position.

--- vendor/assets/javascripts/areacomplete.js
+++ vendor/assets/javascripts/areacomplete.js
@@ -5,8 +5,35 @@
 jQuery at-username
 Autocomplete usernames when typing @
 */
-
 // case insensitive sort
+
+jQuery.fn.extend({
+  insertAtCaret: function(myValue){
+    return this.each(function(i) {
+      if (document.selection) {
+        //For browsers like Internet Explorer
+        this.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+        this.focus();
+      }
+      else if (this.selectionStart || this.selectionStart == '0') {
+        //For browsers like Firefox and Webkit based
+        var startPos = this.selectionStart;
+        var endPos = this.selectionEnd;
+        var scrollTop = this.scrollTop;
+        this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
+        this.focus();
+        this.selectionStart = startPos + myValue.length;
+        this.selectionEnd = startPos + myValue.length;
+        this.scrollTop = scrollTop;
+      } else {
+        this.value += myValue;
+        this.focus();
+      }
+    })
+  }
+});
 
 $(document).ready(function() {
 
@@ -232,7 +259,7 @@ $(document).ready(function() {
         case 32: // space
           active = ddl.find('li.active');
           if (active.length > 0) {
-            textarea.val(textarea.val().substring(0, textarea.data('ac_start') + 1) + active.text() + ' ');
+            textarea.insertAtCaret(active.text());
           }
           removeUsernameAutocomplete(textarea_wrapper);
           return false;
