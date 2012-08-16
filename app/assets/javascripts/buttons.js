49aa331072d2b1ca8c330299d3426fef8a13eefe
Jan Bernacki <releu@me.com>
2012-08-16 22:57:31 +0400

    fix detail page

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -3,10 +3,12 @@ $(function(){
     var post = $(post_html)
     var author = post.data('author')
     var post_data = window.posts[post.data('id')]
-    var rating = window.ratings[author]
+    if (window.ratings) {
+      var rating = window.ratings[author]
     
-    // setup user's rating
-    post.find('span.rating').html(rating)
+      // setup user's rating
+      post.find('span.rating').html(rating)
+    }
     
     // setup edit button
     if (!window.current_user || (window.current_user.username != author && !window.current_user.admin)) {
