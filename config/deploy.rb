f3fec61471a0b1a0414cd91e55711254120cdeb2
Anatoly Makarevich <makaroni4@gmail.com>
2012-09-05 03:13:56 +0400

    Added capistrano tasks for managing unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,9 @@ set :scm, :git
 set :branch, ENV['BRANCH'] || "master"
 set :user, "git"
 
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+
 after "deploy:restart", "deploy:cleanup"
 
 after "deploy:update_code", "db:link_configuration_file"
@@ -42,8 +45,12 @@ end
 
 namespace :deploy do
   task :restart do
-    # this isn't working :(
-    # run "cd #{current_release} && RAILS_ENV=#{deploy_env} bundle exec god -c #{current_release}/config/god.rb"
-    # run "cd #{current_release} && bundle exec god restart unicorn"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{deploy_env} -D; fi"
+  end
+  task :start do
+    run "cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{deploy_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
 end
