ac7b9d35ef80564752a7e27000d0dddac3f9e0d7
Jan Bernacki <releu@me.com>
2012-09-10 12:57:38 +0400

    fix gists

--- app/models/markdown.rb
+++ app/models/markdown.rb
@@ -8,11 +8,14 @@ module Markdown
       :lax_html_blocks,
       :autolink
     ]
-
-    # sanitizing html except images for emoji smiles
-    RedcarpetCompat.new(
-      Sanitize.clean(text, elements: ['img'], attributes: { 'img' => ['src', 'alt', 'title', 'class', 'width', 'height']}), 
-      *options
-    ).to_html.html_safe
+    
+    RedcarpetCompat.new(text, *options).to_html.html_safe
+    
+    # this code brake text
+    # # sanitizing html except images for emoji smiles
+    # RedcarpetCompat.new(
+    #   Sanitize.clean(text, elements: ['img'], attributes: { 'img' => ['src', 'alt', 'title', 'class', 'width', 'height']}), 
+    #   *options
+    # ).to_html.html_safe
   end
 end
