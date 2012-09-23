07464cf2a3a1c90bf07cf78313571c94e9467103
Anatoly Makarevich <makaroni4@gmail.com>
2012-09-23 16:19:34 +0400

    FIX rspec

--- spec/models/time_counter_spec.rb
+++ spec/models/time_counter_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe TimeCounter do
-  let(:time_counter) { Factory :time_counter }
+  let(:time_counter) { create :time_counter }
   subject { time_counter }
 
   it { should be_valid }
