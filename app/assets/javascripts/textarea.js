c938435521cdd363186fd77ace35ef8664e58811
Anatoly Makarevich <makaroni4@gmail.com>
2012-09-23 18:08:54 +0400

    FIX textarea autoresize trigger

--- app/assets/javascripts/textarea.js
+++ app/assets/javascripts/textarea.js
@@ -1,10 +1,12 @@
 $(function(){
-  $('textarea').autoResize()
+  $('textarea').autoResize()  
   $('textarea').tabby({tabString: '  '})
   $('textarea').keydown(function (e) {
    if ((e.ctrlKey || e.metaKey) && e.keyCode == 13) {
      $(this).parents('form:first').submit()
    }
   })
-  $('textarea.at-username').atUsername();
+  $('textarea.at-username').atUsername();  
+  
+  $('textarea').trigger('input.resize');
 });
