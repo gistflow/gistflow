f9721cc24cd510871adf2f29a4c3f09e7e95f88b
Anatoly Makarevich <makaroni4@gmail.com>
2012-09-28 01:10:55 +0400

    FIX rake task for capistrano, seems like we need migrations for cap :)

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,6 +33,11 @@ namespace :app do
   task :console, :roles => :app do
     exec %{ssh #{domain} -t "#{default_shell} -c 'cd #{current_path} && bundle exec rails c #{rails_env}'"}
   end
+
+  desc "remote rake task"
+  task :rake do
+    run "cd #{deploy_to}/current; RAILS_ENV=#{rails_env} rake #{ENV['TASK']}"
+  end
 end
 
 namespace :deploy do
@@ -52,13 +57,3 @@ namespace :deploy do
 end
 
 after 'deploy:finalize_update', 'deploy:make_symlinks'
-
-# useful tasks
-
-# cap production rake TASK=users:fetch_locations
-namespace :rake do
-  desc "remote rake task"  
-  task :default do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake #{ENV['TASK']}"
-  end
-end
