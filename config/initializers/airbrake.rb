8f89a2de74462b88657cc612c299336c62418fae
Jan Bernacki <releu@me.com>
2012-10-08 18:40:41 +0400

    move to errbit

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = Configuration.airbrake.api_key
+  config.host    = 'errbit.vm.evrone.ru'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
