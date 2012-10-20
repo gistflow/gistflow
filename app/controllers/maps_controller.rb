e2276bd1e7f0ec9163850140e6e849010ffe3bee
Jan Bernacki <releu@me.com>
2012-10-20 17:26:47 +0400

    refactoring and mark up

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,16 +1,5 @@
 class MapsController < ApplicationController
   def show
-    user_ids = []
-    @locations = Location.ready.group_by do |location|
-      user_ids << location.locationable_id
-      [location.lat, location.lng]
-    end
-    users = Hash[User.find(user_ids).to_a.map { |u| [u.id, u] }]
-    @locations = @locations.to_a.map do |positions, locations|
-      { count: locations.size,
-        users: locations.map { |l| users[l.locationable_id].username },
-        lat: positions[0],
-        lng: positions[1] }
-    end
+    @locations = Location.ready.to_map
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -11,7 +11,18 @@ class Location < ActiveRecord::Base
   
   attr_accessible :address
   
-  def as_json(options)
-    { label: locationable.username, lat: lat, lng: lng }
+  def self.to_map
+    user_ids = []
+    locations = scoped.group_by do |location|
+      user_ids << location.locationable_id
+      [location.lat, location.lng]
+    end
+    users = Hash[User.find(user_ids).to_a.map { |u| [u.id, u] }]
+    locations.to_a.map do |positions, locations|
+      { count: locations.size,
+        users: locations.map { |l| users[l.locationable_id].username },
+        lat: positions[0],
+        lng: positions[1] }
+    end
   end
 end
