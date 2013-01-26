8b69af87041a2e7c24230f31361a7e559d7fb181
Jan Bernacki <releu@me.com>
2013-01-26 21:36:29 +0400

    hard fix unescaped js

--- app/models/markdown.rb
+++ app/models/markdown.rb
@@ -1,6 +1,15 @@
 module Markdown
   def self.markdown(text)
-    html = ::GitHub::Markdown.render_gfm(text)
+    text = Sanitize.clean(text)
+    rendered = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+        :autolink            => true,
+        :fenced_code_blocks  => true,
+        :lax_spacing         => true,
+        :filter_html         => true,
+        :gh_blockcode        => true,
+        :hard_wrap           => true
+    )
+    html = rendered.render(text)
     
     doc = Nokogiri::HTML(html)
     doc.search("//pre[@lang]").each do |pre|
