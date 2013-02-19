83b11d3d0f28793a63f794f33d522d328f5f7b88
Anatoli Makarevich <makaroni4@gmail.com>
2013-02-19 22:45:25 +0300

    Added tag cloud page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,6 @@
 class TagsController < ApplicationController
+  layout :choose_layout
+
   def show
     if @tag = Tag.find_by_name(params[:id])
       redirect_to @tag.entity if @tag.alias?
@@ -8,6 +10,20 @@ class TagsController < ApplicationController
         .find_or_initialize_by_tag_id(@tag.id) if user_signed_in?
     else
       render 'search/nothing'
+    end
+  end
+
+  def cloud
+    @tags = Tag.for_cloud
+  end
+
+  private
+  def choose_layout
+    case action_name
+    when 'cloud'
+      'sidebarless'
+    else
+      'application'
     end
   end
 end
