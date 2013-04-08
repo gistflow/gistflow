f083e9cbe955f7fd5cb60acae7abfd4cf48a838a
Jan Bernacki <releu@me.com>
2013-04-08 21:22:01 +0400

    try code mirror, fix back to flow

--- app/assets/javascripts/new_application.js
+++ app/assets/javascripts/new_application.js
@@ -3,3 +3,13 @@
 //= require underscore
 //= require layout
 //= require cache
+//= require codemirror
+
+$(document).ready(function(){
+  var myCodeMirror = CodeMirror.fromTextArea($('textarea#content')[0], {
+    tabSize: 2,
+    mode: 'gfm',
+    lineNumbers: false,
+    theme: 'default'
+  })
+})
