8ef39668109c0889734bb146043fbb0f2f9b2a35
Jan Bernacki <releu@me.com>
2013-12-18 20:32:43 +0300

    Update omnauth.rb

--- config/initializers/omnauth.rb
+++ config/initializers/omnauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Configuration.omniauth.github.key, Configuration.omniauth.github.secret, scope: 'gist', client_options: { ssl: { ca_path: "/etc/ssl/certs" } }
+  provider :github, Configuration.omniauth.github.key, Configuration.omniauth.github.secret, client_options: { ssl: { ca_path: "/etc/ssl/certs" } }
 end
